c34924c78e0fa5d87e232387b8fa81c10ad93b1e
Tamashi-Dake <87293433+Tamashi-Dake@users.noreply.github.com>
2023-04-20 00:28:57 +0700

    Start adding Employee tools

---
+++ QuanLyTheXe/theXe.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
+    />
+    <title>ADEPRO</title>
+	 
+</head>
+<body>
+<style>
+    <?php
+    include "../guessIndex.css";
+    include "../bootstrap.css"
+    ?>
+    
+</style>
+<?php include "../modules/Eheader.html" ?>
+	<main>
+
+      <!-- <div class="search-bar-wapper">
+        <form class="search-bar" action="">
+          <input
+            type="text"
+            placeholder="Nhập vào biển số hoặc ID thẻ"
+            name="search"
+          />
+          <button type="submit"><i class="fa fa-search"></i></button>
+        </form>
+      </div> -->
+    </main>
+    <?php include "../modules/feedback-btn.html" ?>
+    <?php include "../modules/footer.html" ?>
+</body>
+</html>
