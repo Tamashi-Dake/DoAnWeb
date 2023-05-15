35554debb849ac1a81f18ce7a7e94dc685174cc7
Tamashi-Dake <87293433+Tamashi-Dake@users.noreply.github.com>
2023-05-15 23:43:30 +0700

    update img path for admin page
    remove echo $conn

--- modules/pages/QuanLyTaiKhoan/Account.php
+++ modules/pages/QuanLyTaiKhoan/Account.php
@@ -52,26 +52,8 @@
           <!-- Default box -->
           <div class="card">
             <div class="card-header">
-              <h3 class="card-title">Projects</h3>
-
-              <div class="card-tools">
-                <button
-                  type="button"
-                  class="btn btn-tool"
-                  data-card-widget="collapse"
-                  title="Collapse"
-                >
-                  <i class="fas fa-minus"></i>
-                </button>
-                <button
-                  type="button"
-                  class="btn btn-tool"
-                  data-card-widget="remove"
-                  title="Remove"
-                >
-                  <i class="fas fa-times"></i>
-                </button>
-              </div>
+              <h3 class="card-title">Danh sách tài khoản</h3>
+             
             </div>
             <div class="card-body p-0">
               <table class="table table-striped projects">
@@ -84,7 +66,7 @@
                     <th style="width: 10%" class="text-center">CCCD</th>
                     <th style="width: 10%" class="text-center">Giới tính</th>
                     <th style="width: 20%" class="text-center">
-                    <a class="btn btn-success btn-sm " href="project-add.php">
+                    <a class="btn btn-success btn-sm " href="Account-add.php">
                         <i class="fas fa-plus"> </i>
                         Thêm
                       </a>
@@ -113,7 +95,7 @@
                     <td>" . $row["identityCard"] . "</td>
                     <td>" . $row["sex"] . "</td>
                     <td class='project-actions text-center'>
-                      <a class='btn btn-info btn-sm' href='project-edit.php'>
+                      <a class='btn btn-info btn-sm' href='Account-edit.php'>
                         <i class='fas fa-pencil-alt'> </i>
                         Sửa
                       </a>
@@ -164,9 +146,7 @@
     <script src="../../plugins/jquery/jquery.min.js"></script>
     <!-- Bootstrap 4 -->
     <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- AdminLTE App -->
+    <!-- AdminLTE theme -->
     <script src="../../dist/js/adminlte.min.js"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="../../dist/js/demo.js"></script>
   </body>
 </html>
