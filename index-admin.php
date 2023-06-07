875783f52c093a8f0ee8481b6a169f779791998f
Tamashi-Dake <87293433+Tamashi-Dake@users.noreply.github.com>
2023-06-07 21:32:14 +0700

    Add demo search result for index

--- index-admin.php
+++ index-admin.php
@@ -59,6 +59,48 @@
         </form>
       </div>
 
+      <?php include "connection.php"; ?>
+      <?php
+    if(!isset($_POST['search'])){
+      $_POST['search'] = "";
+    }
+    else{
+      $search = $_POST['search']; 
+      $sql_search = "select * from webbaiguixe.vehicleinout inner join  webbaiguixe.parkitem on vehicleinout.cardID=parkitem.cardID where vehicleinout.display=1 and ( vehicleinout.cardID='".$search."' or vehicleinout.licensePlate='".$search."') and type=1 order by vehicleinout.vehicleInOutID desc limit 1";
+      $rs=$conn->query($sql_search);
+    ?>
+      <?php 
+      if($rs->num_rows > 0){ 
+        while($row = $rs->fetch_assoc()) {
+          // id và biển số cùng 1 dòng với nhau
+          // areaName vs location cùng dòng
+          // css cho đẹp đẹp tí,để nó dưới thanh tìm kiếm
+          //chỉnh cho 1 file r copy nó sang index-guess và admin là được
+          ?>
+          <div class="searchResult">        
+            <p >
+            <i class="fa-regular fa-id-badge"></i>ID thẻ: <?php echo $row['cardID'] ?> 
+            </p>
+            <p>
+            <i class="fa-sharp fa-solid fa-cars"></i>Biển số: <?php echo $row['licensePlate']?> 
+            </p>
+            <p>
+            <i class="fa-solid fa-building-user"></i>Khu: <?php echo  $row['areaName']?> 
+            </p>
+            <p>
+            <i class="fa-solid fa-map-location-dot"></i>Ô đỗ: <?php echo  $row['location']?> 
+            </p>
+          </div>
+          <?php
+          }
+      }
+      else{
+        // $_SESSION['rp_error'] = "ko có xe trong bãi";
+        // echo $_SESSION['rp_error'];
+        // unset($_SESSION['rp_error']);
+      }
+    }
+      ?>
     </main>
     <?php include "./modules/footer.html" ?>
 </body>
--- index-employee.php
+++ index-employee.php
@@ -20,7 +20,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link
       rel="stylesheet"
-      href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
     />
     <title>ADEPRO</title>
 	 
@@ -33,23 +33,29 @@
       html,body {
   margin:0;
   /* overflow-y:hidden; */
-}
+  }
     <?php
     include "guessIndex.css";
     include "bootstrap.css"
     ?>
-    
+    .searchResult{
+      display:grid;
+      grid-template-columns: repeat(2,1fr);
+      background:var(--search-bar-color);
+      position: absolute;
+      top: 65%;
+      left: 25%;
+      min-width: 50%;
+      border-radius: 10px;
+    }
+    .searchResult p{
+      padding: 20px;
+      margin-bottom:0;
+    }
 </style>
-<?php include "./modules/header-navbar/header-employee.html";
+<?php   
+ include "./modules/header-navbar/header-employee.html";
 ?>
-<!--  
-	<a href="logout.php">Logout</a>
-	<h1>This is the index page</h1>
-
-	<br>
-	Hello, <?php 
-	// echo $user_data['user_name']; ?> -->
-
 	<main>
 
       <div class="search-bar-wapper">
@@ -62,6 +68,48 @@
           <button type="submit"><i class="fa fa-search"></i></button>
         </form>
       </div>
+<?php include "connection.php"; ?>
+      <?php
+    if(!isset($_POST['search'])){
+      $_POST['search'] = "";
+    }
+    else{
+      $search = $_POST['search']; 
+      $sql_search = "select * from webbaiguixe.vehicleinout inner join  webbaiguixe.parkitem on vehicleinout.cardID=parkitem.cardID where vehicleinout.display=1 and ( vehicleinout.cardID='".$search."' or vehicleinout.licensePlate='".$search."') and type=1 order by vehicleinout.vehicleInOutID desc limit 1";
+      $rs=$conn->query($sql_search);
+    ?>
+      <?php 
+      if($rs->num_rows > 0){ 
+        while($row = $rs->fetch_assoc()) {
+          // id và biển số cùng 1 dòng với nhau
+          // areaName vs location cùng dòng
+          // css cho đẹp đẹp tí,để nó dưới thanh tìm kiếm
+          //chỉnh cho 1 file r copy nó sang index-guess và admin là được
+          ?>
+          <div class="searchResult">        
+            <p >
+            <i class="fa-regular fa-id-badge"></i>ID thẻ: <?php echo $row['cardID'] ?> 
+            </p>
+            <p>
+            <i class="fa-sharp fa-solid fa-cars"></i>Biển số: <?php echo $row['licensePlate']?> 
+            </p>
+            <p>
+            <i class="fa-solid fa-building-user"></i>Khu: <?php echo  $row['areaName']?> 
+            </p>
+            <p>
+            <i class="fa-solid fa-map-location-dot"></i>Ô đỗ: <?php echo  $row['location']?> 
+            </p>
+          </div>
+          <?php
+          }
+      }
+      else{
+        // $_SESSION['rp_error'] = "ko có xe trong bãi";
+        // echo $_SESSION['rp_error'];
+        // unset($_SESSION['rp_error']);
+      }
+    }
+      ?>
       
     </main>
     <?php include "./modules/footer.html" ?>
